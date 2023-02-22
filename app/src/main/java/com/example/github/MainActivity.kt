f24daa9c77cc6070ef04dadd05453be9a6427ae1
ashwinijad <ashwini01.jadhav@gmail.com>
2023-02-22 16:17:31 +0530

    commit 1.2

--- app/src/main/java/com/example/github/MainActivity.kt
+++ app/src/main/java/com/example/github/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.e("test","1234")
+        Log.e("test","12345")
     }
 }
